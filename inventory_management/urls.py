45034d53799cbfdf3b7bd1e7a76fbdbe053fe472
stvz233 <zyning1118@outlook.com>
2026-03-20 12:58:02 -0400

    ✨ feat: implemented the front-end web page allowing users to browse products by searching key words (name or description or tag)

--- inventory_management/urls.py
+++ inventory_management/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from products.views import product_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", product_list, name="product_list"),
 ]
